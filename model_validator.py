0c2e3e9482629df315f5a85209514c9111f53cee
Nishant Sharma <iamnishant32@gmail.com>
2026-05-26 15:47:56 +0530

    updated

---
+++ model_validator.py
@@ -0,0 +1,26 @@
+from pydantic import BaseModel, model_validator
+from typing import Dict
+
+class patient(BaseModel):
+    age : int
+    contacts : Dict[str, str]
+
+    @model_validator(type = "after")
+    @classmethod
+    def check(cls, model):
+        if model.age > 60 and "emergency" not in model.contacts:
+            raise ValueError("u didnot provided  the emergency contact number")
+        return model.contacts
+        
+    
+
+data = {"age" : 45, "contacts" : {"address" : "patel nagar", "emergency" : "5986958"}}
+
+p1 = patient(**data)
+
+def printAll(p : patient):
+    print(p.age)
+    print(p.contacts["emergency"])
+
+
+printAll(p1)
