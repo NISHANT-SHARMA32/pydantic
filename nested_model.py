db1c085b523844f1ff977c926e57f060c9f1f8e2
Nishant Sharma <iamnishant32@gmail.com>
2026-05-26 16:33:12 +0530

    final commit

--- nested_model.py
+++ nested_model.py
@@ -16,4 +16,18 @@ a1 = Address(**add_temp)
 data = {"name" : "nishant", "age" : 30, "address" : a1}
 p1 = Patient(**data)
 
-print(p1.address.city)
+temp = p1.model_dump_json()
+print(temp)
+print(type(temp))
+
+
+#this also gives us the power to control what things we want to export
+
+temp1 = p1.model_dump(include = ["name"])
+print(temp1)
+print(type(temp1))
+
+#another you can aslo use exclude
+temp2 = p1.model_dump(exclude = ["name"])
+print(temp2)
+print(type(temp2))
